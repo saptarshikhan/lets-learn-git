ee26dd373930f93f0b3662a2511ec65b566fe364
Saptarshi Khan <saptarshikhan2003@gmail.com>
2022-10-29 15:31:52 +0530

    git initialized

---
+++ diff.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 20, b = 10;
+    cout <<"The difference  is"<< (a - b) << endl;
+}
---
+++ sum.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 20, b = 10;
+    cout <<"The sum is"<< (a + b) << endl;
+}
